85ef556f1d2ae107c2e6a9ba59f311b54375bb6d
Elif Uçhan <uchan-elif@hotmail.com>
2020-06-19 16:57:12 +0300

    screens

---
+++ react-mobil-client/routes/homeStack.js
@@ -0,0 +1,30 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import Home from '../screens/home';
+import Hakkinda from '../screens/Hakkinda';
+import Map from '../Harita'
+
+const screens = {
+  Home: {
+    screen: Home,
+  },
+  Hakkinda: {
+    screen: Hakkinda,
+  },
+  Map: {
+    screen: Map,
+  }
+  
+  
+};
+
+
+
+// home stack navigator screens
+const HomeStack = createStackNavigator(screens);
+
+
+
+export default createAppContainer(HomeStack);
+
+
