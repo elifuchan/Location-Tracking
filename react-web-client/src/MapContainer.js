cbf4ce005b2c8127c1f0207a6f96b4f42813fd30
Elif Uçhan <uchan-elif@hotmail.com>
2020-06-19 16:48:24 +0300

    react web final

---
+++ react-web-client/src/MapContainer.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import {GoogleApiWrapper, Map, Marker} from 'google-maps-react';
+
+import mqtt from 'mqtt';
+
+export class MapContainer extends React.Component {
+	state = {
+		lat: -34.397, 
+		lng: 150.644,
+		altitude: 0,
+		speed: 0, 
+		course: 0
+	}
+
+	updateMap(message){
+		var [lat, lng, altitude, speed, course] = message.toString().split(',');
+
+		this.setState({
+			lat,
+			lng,
+			altitude,
+			speed, 
+			course
+		})
+	}
+
+	componentDidMount() {
+		var client = mqtt.connect('ws://3.15.28.103:3000');
+
+		client.subscribe('LOCATION');
+		client.subscribe('TEMP');
+
+		client.on('connect', function() {
+			console.log('connected!');
+		});
+
+		client.on('message', (topic, message) => {
+			console.log(topic, ' : ', message.toString());
+			switch (topic) {
+				case 'LOCATION':
+					this.updateMap(message);
+					break;
+				case 'TEMP':
+					document.getElementById('TEMP').innerHTML = message.toString();
+					break;
+				default:
+					break;
+			}
+
+			
+		});
+
+
+	}
+	
+	render(){
+		const {lat,lng, altitude, speed, course } = this.state;
+		return(
+			<div style={{ width: '100%', height: '100%' }}>
+				<Map
+          google={this.props.google}
+          initialCenter={{
+						lat, 
+						lng
+					}}
+					center={{
+						lat,
+						lng
+					}}
+          zoom={18}
+        >
+					<Marker
+						icon= {{
+							path: window.google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+							fillColor: 'blue',
+							fillOpacity: 0.8,
+							strokeWeight: 3,
+							scale: 7,
+							rotation: parseFloat(course),
+						}}
+						position={{ lat, lng }}
+					/>
+				</Map>
+
+				<div id="info">
+					<h2>{speed} km/h</h2>
+					
+					<h2><span id="TEMP"> </span>°C</h2>
+
+					<h2>{altitude} msl</h2>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyCJ7QPj_9a3CJbnF20tMK4JA_3pvRpjMfY")
+})(MapContainer)
